5a2c7a606c342c9fe92aa9008c32a74ba0734213
haxmanster <grzegorz-wolyniec@o2.pl>
2018-04-16 20:32:10 +0200

    Change front end

--- main.py
+++ main.py
@@ -20,13 +20,15 @@ def get_info():
 
   def limit():
     if godzina > 24:
-      return "Nie prawidłowy format czasu. Podaj zakres godzin od 0 do 24"
+      return "Nie prawidłowy format czasu. Podaj zakres godzin od 0 do 24 "
     if minuta > 60:
-      return "Nie prawidłowy format czasu. Podaj zakres minut 0d 0 do 60"
+      return "Nie prawidłowy format czasu. Podaj zakres minut 0d 0 do 60 "
     if godzina < 0:
       return "Nie moża cofnąć sie w czasie !!!"
     if minuta < 0:
       return "Nie można cofnąć się w czasie !!! "
+    if godzina  ==str(''):
+      return "Podałes pusty kat"
     return powyzej_12()
 
   def powyzej_12():
@@ -49,10 +51,10 @@ def get_info():
   def wynik():
     return limit()
 
-  return render_template('results.html', the_title='Wynik:',the_minute=minuta,the_hour=godzina,the_result=wynik())
+  return render_template('results.html', the_title='RESULT',the_minute=minuta,the_hour=godzina,the_result=wynik())
 
 @app.route('/entry')
 def strona_powitalna():
     return render_template("entry.html", the_title='CLOCK ANGLE')
 if __name__ == "__main__":
-  app.run(host='0.0.0.0', debug=False, port=80)
+  app.run(host='0.0.0.0',debug=True)
